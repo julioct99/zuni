b0bbccd6201bb1bd64da096706d3de6355dc09dc
Maddox-s-Lab <dev0frag@gmail.com>
2020-11-08 17:24:23 +0100

    Se han añadido algunos valores de strings. Tambien se ha modificado el layout del fragment del historico. Esta en progreso el controlar aun la accion de buscar un historico para guardarlo.

--- app/src/main/java/es/unex/giis/zuni/ui/historico/HistoricoFragment.java
+++ app/src/main/java/es/unex/giis/zuni/ui/historico/HistoricoFragment.java
@@ -5,27 +5,97 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import es.unex.giis.zuni.R;
 import es.unex.giis.zuni.adapter.HistoricalAdapter;
+import es.unex.giis.zuni.countrycodes.CountryCode;
 import es.unex.giis.zuni.openweather.AppExecutors;
 import es.unex.giis.zuni.openweather.HistoricalNetworkLoaderRunnable;
 
 public class HistoricoFragment extends Fragment {
+    private EditText EditText_city;
+    private Spinner spinner1, spinner2;
+    private Button button1, button2;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
 
     private HistoricalAdapter adapter;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+
+
+    public void act1(View v){
+        String cityname = EditText_city.getText().toString();
+
+        if (cityname.equals("")){
+            Snackbar.make(v, getString(R.string.Historical_save_err1_msg) + " " + cityname, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.i("Historico", "Se ha pulsado el boto de guardar historico por nombre");
+        Snackbar.make(v, getString(R.string.Historical_save_msg) + " " + cityname, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+
+    public void act2(View v){
+        String cityname = EditText_city.getText().toString();
+        Log.i("Historico", "Se ha pulsado el boto de guardar historico de la localización guardada");
+        Snackbar.make(v, getString(R.string.Historical_save_msg) + " " + cityname, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_historico, container, false);
+        EditText_city = root.findViewById(R.id.et_city);
+        spinner1 = root.findViewById(R.id.spinner1);
+        spinner2 = root.findViewById(R.id.spinner2);
+        button1 = root.findViewById(R.id.button1);
+        button2 = root.findViewById(R.id.button2);
+
+        JsonReader reader = new JsonReader(new InputStreamReader(getResources().openRawResource(R.raw.country_codes)));
+        List<CountryCode> countryCodes = Arrays.asList(new Gson().fromJson(reader, CountryCode[].class));
+        ArrayAdapter<CountryCode> spinnerAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_spinner_item,countryCodes);
+        spinner1.setAdapter(spinnerAdapter);
+        spinner1.setSelection(208);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                act1(v); //Busqueda del historico de "ayer" de la ciudad (indicada en city) y se guarda en la base de datos
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                act2(v); //Busqueda del historico de "ayer" de la ciudad guardada (indicada en spinner2) y se guarda en la base de datos
+            }
+        });
+
+
+
+        //Por defecto se muestran los historicos guardados
+
         Log.e("HOLAAAAAAAAAAAA","3");
         recyclerView = (RecyclerView) root.findViewById(R.id.listHistorical);
         recyclerView.setHasFixedSize(true);
@@ -38,6 +108,8 @@ public class HistoricoFragment extends Fragment {
         ));
         recyclerView.setAdapter(adapter);
 
+
+        Log.i("Historico", "Se ha cargado el fragment del historico");
         return root;
     }
 }
